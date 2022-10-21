9a90a8a7e09fe8582fba2abc5ded8d720bb29abc
Arjun <arjun1709@gmail.com>
2022-10-22 01:06:15 +0530

    Functionality to get the nonEVM native asset details

---
+++ src/chains/non-evm/solana.js
@@ -0,0 +1,9 @@
+module.exports = {
+    SYMBOL: 'SOL',
+    NAME: 'Solana',
+    DECIMALS: 9,
+    ASSET_API: 'https://api.coingecko.com/api/v3/coins/solana',
+    FUNCTIONALITY_SUPPORT: {
+      getNativeAssetDetails: true,
+    },
+};
---
+++ src/chains/non-evm/tron.js
@@ -0,0 +1,9 @@
+module.exports = {
+    SYMBOL: 'TRX',
+    NAME: 'Tron',
+    DECIMALS: 6,
+    ASSET_API: 'https://api.coingecko.com/api/v3/coins/tron',
+    FUNCTIONALITY_SUPPORT: {
+      getNativeAssetDetails: true,
+    },
+};
